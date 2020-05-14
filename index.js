08e2fee493a38942ef4108ae803a029be6f1f95e
Rohit Salaria <rohit.salaria@jungleworks.com>
2020-05-14 22:52:50 +0530

    package version 1.0.1

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 #! /usr/bin/env node
-require('./lib/ng-rok');
+let { startProcess } = require('./lib/tunnel-local');
+
+
+
+const fileName = process.argv[2];
+const envName = process.argv[3];
+if (!fileName) {
+	throw new Error('file name argument missing');
+}
+
+const paths = __dirname.split('node_module');
+let rootPath = __dirname;
+
+if(paths.length === 2){
+	rootPath = paths[0];
+}
+
+const absolutePath = rootPath + `/${fileName}`
+startProcess(absolutePath, envName)
